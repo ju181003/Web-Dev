b6e48b677b6985092d70e5691a864f4a8d0a495d
Julian <ju181003@gmail.com>
2023-10-02 13:10:58 -0600

    express

---
+++ Express-test/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+app.get("/",(req, res) =>{
+    res.write("Hello world")
+    res.send();
+});
+
+app.listen(3000, () =>{
+    console.log("Server is running on port 3000");
+});
