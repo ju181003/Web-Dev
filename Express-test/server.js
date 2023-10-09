1ba8e50efd7202078d28e1495ffdf0f0e44ff090
Julian <ju181003@gmail.com>
2023-10-09 12:47:47 -0600

    9-oct_EJS, GET/POST

--- Express-test/server.js
+++ Express-test/server.js
@@ -7,31 +7,56 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static("public"));
 
+app.engine("ejs", require("ejs").renderFile);
 
-app.get("/",(req, res) =>{
-    res.write("Hello world");
-    res.send();
-});
+app.set("view engine", "ejs");
 
-app.get("/hello",(req, res) =>{
+
+
+
+app.get("/hellow",(req, res) =>{
+    res.setHeader("Content-Type", "text/html");
     res.write("<h1>Hello again back to you</h1>");
     res.write("<ul><li>Item 1</li><li>Item 2</li></ul>");
 
     res.send();
 });
 
-app.get("/product",(req, res) =>{
+app.get("/",(req, res) =>{
     console.log(__dirname);
-    res.sendFile(__dirname + "/public/html/index.html");
+    res.render("index",{ name: "julian"});
     
 });
 
-app.get("/sellProduct",(req, res) =>{
-    var prodName = req.query.name;
-    res.write("This is a great product(express) buy " + prodName + "");
+app
+.route("/sellProduct")
+.get((req, res) => {
+    var prodName = req.query.productName;
+    res.write("This is a great product from get method(express) buy " + prodName + "");
+    res.send();
+    
+})
+.post((req, res) =>{
+    var prodName = req.body.productName;
+    res.write("This is a great product from post method (express) buy " + prodName + "");
+    res.send();
+    
+}).put().delete().patch();
+
+app.get("/sellProduct/:productName.:model/detail",(req, res) =>{
+    var prodName = req.params.productName;
+    var model = req.params.model;
+    res.write("This is a great product from get method V2(express) buy " + 
+    prodName + 
+    " " + 
+    model  
+    
+    );
     res.send();
     
 });
+
+
 
 
 app.listen(3000, () =>{
